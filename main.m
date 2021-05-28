5413f37de27e2fc325660bbcf94ec48a89324067
Slex <alex22_7@hotmail.com>
2021-05-28 12:11:06 +0200

    Number one

---
+++ main.m
@@ -0,0 +1,24 @@
+//
+//  main.m
+//  uroswm
+//
+//  Created by Alessandro Sangiuliano on 22/06/20.
+//  Copyright (c) 2020 Alessandro Sangiuliano. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <XCBKit/XCBConnection.h>
+
+
+int main(int argc, const char * argv[])
+{
+    @autoreleasepool
+    {
+        // insert code here...
+        NSLog(@"Starting Puck Dock...");
+        XCBConnection *connection = [XCBConnection init];
+
+    }
+
+    return 0;
+}
